4634718d5f6c021c54c851b231fba8608656e700
TristanK <frostflag@gmail.com>
2018-11-28 00:31:28 -0500

    changed db settings back

--- django/django_test/mytestsite/mytestsite/settings.py
+++ django/django_test/mytestsite/mytestsite/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'mytestsite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.postgresql',
-     #   'NAME': 'blackandbackdb',
-     #   'USER': 'postgres',
-     #   'PASSWORD': '#28546793lstR',
-     #   'HOST': 'localhost',
-     #   'PORT': '5432',
      'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'blackandbackdb',
+         'USER': 'postgres',
+         'PASSWORD': '#28546793lstR',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
     
 }
