6d46062c328c33ee91846eaac21e7ad341f9c373
TristanK <frostflag@gmail.com>
2018-11-28 00:28:16 -0500

    added contact,aboutus,tophotos skeletons

--- django/django_test/mytestsite/home/urls.py
+++ django/django_test/mytestsite/home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$' , views.index, name='index'),
+	url('aboutus', views.aboutus, name='aboutus'),
+	url('contact', views.contact, name='contact'),
+	url('top_photos', views.top_photos, name='top_photos'),
+
 ]
