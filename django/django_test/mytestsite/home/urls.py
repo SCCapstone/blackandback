35ca1c81f65c9e6b8a4f6342c2b41391de11935b
allisonrrogers <rogers2011ala@gmail.com>
2019-02-25 21:34:49 -0500

    i think these are all the files I've changed regarding profile

--- django/django_test/mytestsite/home/urls.py
+++ django/django_test/mytestsite/home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	url('aboutus', views.aboutus, name='aboutus'),
 	url('contact', views.contact, name='contact'),
 	url('top_photos', views.top_photos, name='top_photos'),
+	url(r'^profile', views.profile, name='profile'),
 
 ]
