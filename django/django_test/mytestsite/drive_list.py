8513993d165ea452f2df592bc211cad6a1a74cba
TKlintworth <frostflag@gmail.com>
2019-04-06 15:47:11 -0400

    cleaned up drive_list.py

--- django/django_test/mytestsite/drive_list.py
+++ django/django_test/mytestsite/drive_list.py
@@ -20,7 +20,6 @@ except ImportError:
     flags = None
 
 
-
 CLIENT_SECRETS = 'client_secrets.json'
 SCOPE = 'https://www.googleapis.com/auth/drive'
 STORAGE = Storage('credentials.storage')
@@ -28,28 +27,16 @@ STORAGE = Storage('credentials.storage')
 def upload_photo(imgName):
 
     credentials = STORAGE.get()
-    
-    #media = discovery.MediaFileUpload(imgName, mimetype='image/png',chunksize=-1)
-    #credentials = None
-    #flow = client.flow_from_clientsecrets('client_secrets.json',SCOPE)
-    #flow.redirect_uri = '/upload.html'
-
-    
-    #storage = Storage('storage.json')
-
-    #credentials = tools.run_flow(flow,storage)
-    #storage.put(credentials)
-    #credentials = storage.get()
 
     if not credentials or credentials.invalid:
         #flow = client.flow_from_clientsecrets('client_id.json', SCOPE)
         flow = client.flow_from_clientsecrets(CLIENT_SECRETS, scope=SCOPE)
         #http = httplib2.Http()
+        flow.redirect_uri = "http://127.0.0.1:8000/upload/"
         if flags:
             #credentials = run_flow(flow, STORAGE, http=http)
             credentials = tools.run_flow(flow, STORAGE, flags)
     #return credentials
-    
 
     http = httplib2.Http()
     http = credentials.authorize(http)
@@ -66,51 +53,3 @@ def upload_photo(imgName):
         res = drive.files().create(body=metadata,media_body=filename).execute()
         if res:
             print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
-
-
-"""
-def upload_photo(imgName):
-    #SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
-    SCOPES = 'https://www.googleapis.com/auth/drive'
-    store = file.Storage('storage.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
-    #
-    #files = DRIVE.files().list(pageSize=87).execute().get('files', [])
-    
-    #imgName = 'testImage.jpg'
-    FILES = (
-        (imgName,None),
-        (imgName, 'application/vnd.google-apps.photo'),
-    )
-    for filename, mimeType in FILES:
-        metadata = {'name':filename}
-        if mimeType:
-            metadata['mimeType'] = mimeType
-        res = DRIVE.files().create(body=metadata, media_body = filename).execute()
-        if res:
-            print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
-"""
-
-#for f in files:
-#    print(f['name'], f['mimeType'])
-
-'''POST https://www.googleapis.com/upload/drive/v3/files?uploadType=media HTTP/1.1
-Content-Type: image/jpeg
-Content-Length: [NUMBER_OF_BYTES_IN_FILE]
-Authorization: Bearer [YOUR_AUTH_TOKEN]
-
-[JPEG_DATA]'''
-
-'''
-file_metadata = {'name': 'photo.jpg'}
-media = MediaFileUpload('files/photo.jpg',
-                        mimetype='image/jpeg')
-file = drive_service.files().create(body=file_metadata,
-                                    media_body=media,
-                                    fields='id').execute()
-print ("File ID: %s" % file.get('id'))
-'''
